538d27f9216d02ec74da649c6a4fbf8b2b029ba3
Barbara Miller <galgeek@me.com>
2022-01-19 16:53:46 -0800

    minimal yt-dlp updates

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 '''
 setup.py - brozzler setup script
 
-Copyright (C) 2014-2021 Internet Archive
+Copyright (C) 2014-2022 Internet Archive
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
